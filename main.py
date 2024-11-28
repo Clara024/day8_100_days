b0252a6029829eaca747071db41e7c11a090a1fb
Izundu Clara Chinecherem <ci.chinecherem@stu.unizik.edu.ng>
2024-11-28 18:45:18 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+gender = input("Enter your gender: ")
+if gender== "male" or gender == "Male":
+  print("You are a strong man")
+  height = input ("Are you tall or short? ")
+  if height == "tall":
+    print("  That is very nice")
+  else:
+    print("You are good to go")
+elif gender == "female" or gender == "Female":
+  print("You are a nice lady")
+else:
+  print("Goodluck")
